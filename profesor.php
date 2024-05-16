487d432d117869f9e062afe7cadd5046a76654a5
unknown <luisvegaguerrero722@gmail.com>
2024-05-16 11:26:34 -0500

    ...

--- profesor.php
+++ profesor.php
@@ -7,6 +7,7 @@
   <title>Bootstrap demo</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/qr.css">
 </head>
 
 <body>
@@ -27,6 +28,29 @@
       </div>
     </div>
   </header>
+  <center>
+     <h1>Bienvenidos Estudiantes</h1> 
+      <div class="body">
+
+        <img src="imagenes proyecto/Fisica.png" alt="Fisica"   width= 250\textwidth; > 
+         <h1>Fisica</h1>
+         <a href="/fisica.html"><button type="button" class="btn">Fisica</button></a>
+         <div class="codigos. btn">
+          <div class="codigos.btn">
+        
+       <img src="imagenes proyecto/lenguaje.png" alt="Lenguaje" width= 250\textwidth;>
+         <h1>Lenguaje</h1>
+         <a href="/lenguaje.html"><button  type="button" class="btn">Lenguaje</button></a>
+         <div class="codigos. btn">
+         <div class="codigos.btn">
+      
+       <img src="imagenes proyecto/sociales si.png" alt="Sociales" width= 250\textwidth>
+         <h1>Sociales</h1>
+         <a href="/sociales.html"><button  type="button" class="btn">Sociales</button></a>
+         <div class="codigos. btn">
+         <div class="codigos.btn">
+        </div> 
+    </center>
   
     
 
