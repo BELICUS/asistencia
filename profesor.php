ca92666484baff094f9edea14d09d97e57ac16c3
Ldvega10 <luisvegaguerrero722@gmail.com>
2024-05-17 13:24:23 -0500

    profesror camabiado

--- profesor.php
+++ profesor.php
@@ -27,32 +27,32 @@
 
       </div>
     </div>
-  </header> 
+  </header>
     
   <center>
      <h1>Bienvenidos Estudiantes</h1> 
       <div class="body">
-        <h1>Fisica</h1>
+    
+
         <img src="imagenes proyecto/a fisica.jpg" alt="Fisica"   width= 250\textwidth; > 
+         <h1>Fisica</h1>
+         <a href="/fisica.php"><button type="button" class="btn">Fisica</button></a>
          
-        <a href="fisica.php"><button type="button" class="btn">Fisica</button></a>
-         <div class="codigos. btn">
-        <div class="codigos.btn">
-        
-       <!--<img src="imagenes proyecto/a lenguaje.jpg" alt="Lenguaje" width= 250\textwidth;>
-         <h2>Lenguaje</h2>
+          
+        <br><br>
+       <img src="imagenes proyecto/a lenguaje.jpg" alt="Lenguaje" width= 250\textwidth;>
+         <h1>Lenguaje</h1>
          <a href="lenguaje.php"><button  type="button" class="btn">Lenguaje</button></a>
-         <div class="codigos. btn">
-         <div class="codigos.btn">
+        
+        <br><br>    
       
        <img src="imagenes proyecto/a sociales.jpg" alt="Sociales" width= 250\textwidth>
-         <h3>Sociales</h3>
-         <a href="sociales.php"><button  type="button" class="btn">Sociales</button></a>
-         <div class="codigos. btn">
-         <div class="codigos.btn">
-        </div> -->
+         <h1>Sociales</h1>
+        <a href="/sociales.php"><button  type="submit" class="btn">Sociales</button></a>
+        
+        </div> 
     </center>
-    
+     
 
   
     
